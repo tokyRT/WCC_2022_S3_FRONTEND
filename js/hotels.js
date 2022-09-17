277e1468ea7f2fa1f1e9b479da12199ef50476d9
BastardC <ainamandrine@gmail.com>
2022-09-17 14:27:52 +0300

    First commit

---
+++ js/hotels.js
@@ -0,0 +1,61 @@
+const hotels = [
+    {
+        id: 1,
+        price: '$120',
+        name: 'Archway Inn',
+        image: '../assets/images/hotel1.jpg'
+    },
+    {
+        id: 2,
+        price: '$125',
+        name: "The Connor Hotel",
+        image: '../assets/images/hotel2.jpg'
+    },
+    {
+        id: 3,
+        price: '$130',
+        name: 'Phoenician',
+        image: '../assets/images/hotel3.jpg'
+    },
+    {
+        id: 4,
+        price: '$160',
+        name: 'Grand Private',
+        image: '../assets/images/hotel4.jpg'
+    },
+]
+
+
+const cardMatchesContainer = document.querySelector('.card-matches-container');
+
+
+const showInHtmlMatches = hotels.map((data) => {
+    return `
+            <div class='card'>
+                <div class='flex'>
+                    <img src=${data.image} />
+                    <div>
+                        <h3>${data.name}</h3>
+                        <div class='flex'>
+                            <div class='flex rating'>
+                                <iconify-icon icon="clarity:star-solid" style="color: #ffce31;" width="17" height="16"></iconify-icon>
+                                <p>4.5/5</p>
+                            </div>
+                            <div class='flex'>
+                                <iconify-icon icon="el:map-marker" style="color: white;" width="14" height="14"></iconify-icon>
+                                <p>Moab, UT</p>
+                            </div>
+                        </div>
+                        <p class='price'>
+                            ${data.price}<span>/right</span>
+                        </p>
+                    </div>
+                </div>
+                <div class='bg-orange'>
+                    <iconify-icon icon="bi:arrow-right" style="color: #F49338" width="20" height="20"></iconify-icon>
+                </div>
+            </div>
+        `
+}).join('')
+
+cardMatchesContainer.innerHTML = showInHtmlMatches
---
+++ js/recommended.js
@@ -0,0 +1,49 @@
+const data = [
+    {
+        id: 1,
+        price: '$160',
+        name: 'Spring Hill Suites',
+        rating: '4.5'
+    },
+    {
+        id: 2,
+        price: '$160',
+        name: "Arne's Royal Hawailan ",
+        rating: '4.5'
+    },
+    {
+        id: 3,
+        price: '$160',
+        name: 'Spring Hill Suites',
+        rating: '4.5'
+    },
+]
+
+
+const cardContainer = document.querySelector('.card-container');
+
+const showInHtml = data.map((data) => {
+    return `
+        <div class='card'>
+            <p class='price'>
+                ${data.price}<span>/right</span>
+            </p>
+            <p class='name'>
+                ${data.name}
+            </p>
+            <div class='flex details'>
+                <div class='flex rating'>
+                    <iconify-icon icon="clarity:star-solid" style="color: #ffce31;" width="17" height="16"></iconify-icon>
+                    <p>${data.rating}/5</p>
+                </div>
+                <div class='flex'>
+                    <iconify-icon icon="el:map-marker" style="color: white;" width="14" height="14"></iconify-icon>
+                    <p>Moab, UT</p>
+                </div>
+            </div>
+        </div>
+
+    `
+}).join('')
+
+cardContainer.innerHTML = showInHtml
